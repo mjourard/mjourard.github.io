cf4b79547330ca7bc4aaa887bab191daa11ffd76
Matthew Jourard <mjourard@mail.uoguelph.ca>
2016-09-07 23:50:48 -0400

    Added new functionality to try to hide older content to keep it relevant. Need a way to display it if the user clicks an expand button or something

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-function supportsImports() {
-    return 'import' in document.createElement('link');
+function HideOldContent() {
+    var count = 0;
+    $("time .end").each(function() {
+        if (timeElementIsTooOld($(this))) {
+            $(this).parent.parent.parent.hide();
+        }
+        count++;
+    });
+    console.log("completed looking for old content. found " + count + " time elements");
 }
 
-if (supportsImports()) {
-    // Good to go!
-    console.log("good to go with rel links!");
-} else {
-    // Use other libraries/require systems to load files.
-    console.log("Use other libraries/require systems to load files.");
+function timeElementIsTooOld($time) {
+    var timeVal = new Date($time.attr("datetime"));
+    var cutOffTime = Date.now();
+    cutOffTime.setYear(cutOffTime.getYear() - 1);
+    return cutOffTime.valueOf() > timeVal.valueOf();
 }
